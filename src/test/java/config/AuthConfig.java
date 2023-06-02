f69fc61af25c79dfc36580bd28d11edb6fad9fb9
Irina <iparakhina91@gmail.com>
2023-06-03 00:18:52 +0300

    updated driver

--- src/test/java/config/AuthConfig.java
+++ src/test/java/config/AuthConfig.java
@@ -3,7 +3,7 @@ package config;
 import org.aeonbits.owner.Config;
 
 @Config.Sources({
-        "classpath:auth.properties"
+        "classpath:properties/auth.properties"
 })
 
 public interface AuthConfig extends Config {
